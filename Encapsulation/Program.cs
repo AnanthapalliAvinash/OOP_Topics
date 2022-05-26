1cdc6386febf8965bbb87306bb166901bb4cbbf6
AnanthapalliAvinash <avinashananthapalli11@gmail.com>
2022-05-26 18:32:59 +0530

    Add project files.

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Encapsulation
+{
+    private String studentName;
+    private int studentAge;
+    public String Name
+    {
+        get
+        {
+            return studentName;
+        }
+
+        set
+        {
+            studentName = value;
+        }
+
+    }
+    public int Age
+    {
+
+        get
+        {
+            return studentAge;
+        }
+
+        set
+        {
+            studentAge = value;
+        }
+
+    }
+}
+class EncapDemo
+{
+    static public void Main()
+    {
+        Encapsulation obj = new Encapsulation();
+        obj.Name = "Ankita";
+        obj.Age = 21;
+        Console.WriteLine("Name: " + obj.Name);
+        Console.WriteLine("Age: " + obj.Age);
+    }
+}
